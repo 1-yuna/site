83e669dd2a61300211c52f1e967fb67c8e2f1aaf
1-yuna <97020423+1-yuna@users.noreply.github.com>
2022-06-28 01:56:38 +0900

    Update middle-two.js

--- js/middle-two.js
+++ js/middle-two.js
@@ -1,14 +1,18 @@
-const middleTwoTextRight = document.querySelectorAll(".middle-two-text-right");
+let middleTwoTextRight = document.querySelectorAll(".middle-two-text-right");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
   console.log("scrollY", value);
 
-  if (1551 > value || value > 2214) {
-    middleTwoTextRight[0].style.animation = "disappear 1s ease-in-out forwards";
-    middleTwoTextRight[1].style.animation = "disappear 1s ease-in-out forwards";
+  if (1833 > value || value > 2660) {
+    middleTwoTextRight[0].style.animation =
+      "disappear  1s ease-in-out forwards";
+    middleTwoTextRight[1].style.animation =
+      "disappear  1s ease-in-out forwards";
+    console.log(value);
   } else {
     middleTwoTextRight[0].style.animation = "slide 1s ease-in-out forwards";
     middleTwoTextRight[1].style.animation = "slide 1s ease-in-out forwards";
+    console.log(value);
   }
 });
